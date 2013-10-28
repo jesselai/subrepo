7f7b09e1d8cf1dc885969cf2d28675144e4b2957
Jesse Lai <jlai@sandia.gov>
2013-10-28 14:00:54 -0600

    Added a new file.

--- subfile1.cs
+++ subfile1.cs
@@ -1,2 +1,3 @@
 // This is subfile1 in subrepo in feature-1.0.0 branch
 // feature-1.0.0
+// Added a fix here!!
---
+++ subfile2.cs
@@ -0,0 +1,2 @@
+// This is subfile2 in subrepo in feature-1.0.0 branch
+// feature-1.0.0
